11f3016307d47b49870ddebcc988ab3e6ecf432d
jptaba <92991909+jptaba@users.noreply.github.com>
2021-11-16 09:18:10 -0500

    werwe

--- lesson-3-independent-releases/exercises/travis_ci/server.js
+++ lesson-3-independent-releases/exercises/travis_ci/server.js
@@ -1,12 +1,14 @@
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
-    console.log('Containers rule!');
+  while (true) {
+    console.log("Containers rule!");
     await sleep(5000);
   }
 }
 
 main();
+
+//gyrd
